7e8a244b6b1566b681018463d877775e0ed52afc
jack0799 <roshanshah620@gmail.com>
2020-11-26 16:28:51 +0530

    1st level

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ import java.util.*;
 class test{
     static int[] pos(String move,String pla,String [][]board){
         String chara=move.charAt(0)+""+move.charAt(1)+" "+pla;
-        System.out.println(chara);
+        //System.out.println(chara);
         int []position=new int[2];
         for(int i=0;i<5;i++){
             for(int j=0;j<5;j++){
