3e0275117110cc7ecb39f6fd7291c56108404599
jack0799 <roshanshah620@gmail.com>
2020-11-26 14:31:00 +0530

    first

--- test.java
+++ test.java
@@ -1,1 +1,23 @@
-nkscnk
+import java.util.*;
+class test{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String [][]board=new String[5][5];
+        System.out.println("Player 1 enter order of your pawns");
+        for(int i=0;i<5;i++){
+            board[0][i]=sc.next()+" A";
+        }
+
+        System.out.println("Player 2 enter order of your pawns");
+        for(int i=0;i<5;i++){
+            board[4][i]=sc.next()+" B";
+        }
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++)
+            System.out.print(board[i][j]+" ");
+            System.out.println();
+        }
+        while
+        //for(int i=0)
+    }
+}
