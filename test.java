653a8807283920a8b0cf201b889ce7f3c552e28e
jack0799 <roshanshah620@gmail.com>
2020-11-26 14:53:13 +0530

    third

--- test.java
+++ test.java
@@ -1,5 +1,10 @@
 import java.util.*;
 class test{
+    static int[][] pos(String move){
+        position[0][0]=1;
+        position[0][1]=1;
+        return position;
+    }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         String [][]board=new String[5][5];
@@ -17,7 +22,39 @@ class test{
             System.out.print(board[i][j]+" ");
             System.out.println();
         }
-        while
+        String winner="NO";
+        int chance=1;
+        while(winner=="NO"){
+            if(chance==1){
+                System.out.println("Player 1 enter your move");
+                String move=sc.next();
+                String a=move.charAt(3);
+                int [][]position=new int[0][1];
+                position=pos(move);
+                if(a=="R"){
+
+                }
+                else if(a=="L"){
+
+                }
+                else if(a=="F"){
+
+                }
+                else if(a=="B"){
+
+                }
+                else{
+                    System.out.println("Wrong input please enter again");
+                    continue;
+                }
+
+                chance=2;
+            }
+            else if(chance==2){
+                System.out.println("Player 1 enter your move");
+                chance=1;
+            }
+        }
         
     }
 }
